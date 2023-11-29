9972ce6546e8f4222efd34f67559c39e22495d68
Andy Lim <aclim@cisco.com>
2023-11-29 16:46:30 +0000

    Add ngrok to the container.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,6 +12,18 @@ sudo make install
 which sed
 echo "--> Sed install complete"
 
+# Install ngrok for ingress on Github Codespace
+echo -e "\n--> Installing Ngrok to enable ingress on Github Codespaces...\n"
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
+    sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && \
+    echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | \
+    sudo tee /etc/apt/sources.list.d/ngrok.list && \
+    sudo apt update && \
+    sudo apt install ngrok
+echo "--> Ngrok install complete."
+echo "--> You will need to add your Ngrok token to use it."
+echo "--> ngrok config add-authtoken <token>"
+
 # Configure and build Sphereon demo containers
 echo -e "\n--> Configuring Sphereon containers ...\n"
 cd /workspaces/OID4VC-demo/docker/compose/build
